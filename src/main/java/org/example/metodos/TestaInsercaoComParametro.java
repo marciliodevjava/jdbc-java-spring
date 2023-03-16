eca7af45c7c8db26b4ae1684de8f6289f66a28c9
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-16 15:46:02 -0300

    Atualização

--- src/main/java/org/example/metodos/TestaInsercaoComParametro.java
+++ src/main/java/org/example/metodos/TestaInsercaoComParametro.java
@@ -1,2 +1,40 @@
-package org.example.metodos;public class TestaInsercaoComParametro {
+package org.example.metodos;
+
+import org.example.ConectionFactory;
+import org.example.metodos.domain.Produto;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercaoComParametro {
+
+    public static void main(String[] args) throws SQLException {
+        ConectionFactory conectionFactory = new ConectionFactory();
+        Connection connection = conectionFactory.criaConexao();
+
+        String nomeEnviou = "Celular";
+        String descricaoEnvivou = "Samsung S20";
+
+        String nome = String.format("'%s'", nomeEnviou);
+        String descricao = String.format("'%s'", descricaoEnvivou);
+
+        Statement stm = connection.createStatement();
+        stm.execute("INSERT INTO produto(nome, descricao) VALUES(" + nome + ", " + descricao + ")", Statement.RETURN_GENERATED_KEYS);
+
+        ResultSet rst = stm.getGeneratedKeys();
+
+        while (rst.next()) {
+            Produto produto = new Produto();
+            Integer id = rst.getInt(1);
+            produto.setId(id);
+            String nomeInserido = rst.getString(1);
+            produto.setNome(nomeInserido);
+            String descricaoInserido = rst.getString(1);
+            produto.setDescricao(descricaoInserido);
+            System.out.println(produto.toString());
+        }
+        connection.close();
+    }
 }
