6015f3a19ed3225ef680705ea80b7e948cf118c4
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-22 20:03:49 -0300

    Add Pool de conecxao.

--- src/main/java/org/example/ConectionFactory.java
+++ src/main/java/org/example/ConectionFactory.java
@@ -1,14 +1,28 @@
 package org.example;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConectionFactory {
 
-    public Connection criaConexao() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-                "root", "@Sa45781256");
+    private final String url = "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
+    private final String user = "root";
+    private final String password = "@Sa45781256";
+    public DataSource dataSource;
 
+    public ConectionFactory(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl(url);
+        comboPooledDataSource.setUser(user);
+        comboPooledDataSource.setPassword(password);
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection criaConexao() throws SQLException {
+        return this.dataSource.getConnection();
     }
 }
