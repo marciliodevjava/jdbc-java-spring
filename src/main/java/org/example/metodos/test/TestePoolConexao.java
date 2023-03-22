4fdd103c89295d9c130a982335f8115b65e2633c
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-22 20:39:16 -0300

    Creation Class TestaPoolConexao

---
+++ src/main/java/org/example/metodos/test/TestePoolConexao.java
@@ -0,0 +1,27 @@
+package org.example.metodos.test;
+
+import org.example.ConectionFactory;
+
+import java.sql.SQLException;
+
+public class TestePoolConexao {
+
+    private static ConectionFactory conectionFactory;
+
+    static {
+        try {
+            conectionFactory = new ConectionFactory();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        for(int i = 0; i < 20; i++){
+            conectionFactory.criaConexao();
+            System.out.println("Conexão Nº: " + i );
+        }
+        System.out.println("Fechando Conexão!");
+        conectionFactory.criaConexao();
+    }
+}
