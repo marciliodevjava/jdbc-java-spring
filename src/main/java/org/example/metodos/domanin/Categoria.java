35b1bf3058980d81963476ab57df4ddbfd6f8dbb
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-29 08:13:28 -0300

    Criação Class Categoria

---
+++ src/main/java/org/example/metodos/domanin/Categoria.java
@@ -0,0 +1,23 @@
+package org.example.metodos.domanin;
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
