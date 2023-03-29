9e5e82320d8f5c32fdef5d2903a39ec2929b7bbe
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-29 08:13:52 -0300

    Criação Class TestaListagemDeCagetoria

---
+++ src/main/java/org/example/metodos/test/TestaListagemDeCagetoria.java
@@ -0,0 +1,19 @@
+package org.example.metodos.test;
+
+import org.example.ConectionFactory;
+import org.example.metodos.produto.dao.CategoriaDAO;
+import org.example.metodos.domanin.Categoria;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TestaListagemDeCagetoria {
+
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = new ConectionFactory().criaConexao()) {
+            CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+            List<Categoria> listaCategoria = categoriaDAO.listar;
+        }
+    }
+}
