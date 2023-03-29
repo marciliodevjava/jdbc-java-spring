8fd402a486eebe3fd4c921fa8ed24ebbfb6dac1d
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-29 08:51:03 -0300

    Add metodo listar por categoria

--- src/main/java/org/example/metodos/produto/dao/ProdutoDAO.java
+++ src/main/java/org/example/metodos/produto/dao/ProdutoDAO.java
@@ -1,18 +1,17 @@
 package org.example.metodos.produto.dao;
 
-import org.example.ConectionFactory;
 import org.example.metodos.domain.Produto;
-import org.example.metodos.repository.ProdutoRepository;
+import org.example.metodos.domanin.Categoria;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersistenciaDAO {
+public class ProdutoDAO {
 
     private Connection connection;
 
-    public PersistenciaDAO(Connection connection){
+    public ProdutoDAO(Connection connection){
         this.connection = connection;
     }
 
@@ -57,6 +56,33 @@ public class PersistenciaDAO {
                 }
             }
         }
+        return produtoList;
+    }
+
+    public List<Produto> buscarPorCategoria(Categoria ct) throws SQLException{
+
+        String sql = "SELECT id, nome, descricao FROM produto WHERE categoria_id = ?";
+
+        List<Produto> produtoList = new ArrayList<>();
+        try(PreparedStatement pstm = connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS)){
+            pstm.setInt(1, ct.getId());
+            pstm.execute();
+
+            try(ResultSet rst = pstm.getResultSet()){
+                while (rst.next()){
+                    Produto produto = new Produto();
+                    Integer id = rst.getInt("id");
+                    produto.setId(id);
+                    String nome = rst.getString("nome");
+                    produto.setNome(nome);
+                    String descricao = rst.getString("descricao");
+                    produto.setDescricao(descricao);
+
+                    produtoList.add(produto);
+                }
+            }
+        }
+
         return produtoList;
     }
 }
