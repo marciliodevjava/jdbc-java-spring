10f9396eca7278097c7bcf083157ce49ea2ef8b0
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-04-03 07:43:55 -0300

    Update

--- src/main/java/org/example/metodos/produto/dao/CategoriaDAO.java
+++ src/main/java/org/example/metodos/produto/dao/CategoriaDAO.java
@@ -1,5 +1,6 @@
 package org.example.metodos.produto.dao;
 
+import org.example.metodos.domain.Produto;
 import org.example.metodos.domanin.Categoria;
 
 import java.sql.Connection;
@@ -22,19 +23,52 @@ public class CategoriaDAO {
 
         String sql = "SELECT id, nome FROM categoria";
 
-        try(PreparedStatement pstm = connection.prepareStatement(sql)){
+        try (PreparedStatement pstm = connection.prepareStatement(sql)) {
             pstm.execute();
 
-            try(ResultSet rst = pstm.getResultSet()){
-                while (rst.next()){
-                   Categoria categoria = new Categoria();
-                   categoria.setId(rst.getInt(1));
-                   categoria.setNome(rst.getString(2));
-                   categorias.add(categoria);
+            try (ResultSet rst = pstm.getResultSet()) {
+                while (rst.next()) {
+                    Categoria categoria = new Categoria();
+                    categoria.setId(rst.getInt(1));
+                    categoria.setNome(rst.getString(2));
+                    categorias.add(categoria);
                 }
             }
 
         }
-        return  categorias;
+        return categorias;
+    }
+
+    public List<Categoria> listarComProdutos() throws SQLException {
+        List<Categoria> categorias = new ArrayList<>();
+        Categoria ultima = null;
+
+        String sql = "SELECT c.id, c.nome, p.id, p.nome, p.descricao " +
+                "FROM categoria c " +
+                "INNER JOIN produto p " +
+                "ON c.id = p.categoria_id ";
+
+        try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.execute();
+
+            try (ResultSet rst = pstm.getResultSet()) {
+                while (rst.next()) {
+                    if (ultima == null || !ultima.getNome().equals(rst.getString(2))) {
+                        Categoria categoria = new Categoria();
+                        categoria.setId(rst.getInt(1));
+                        categoria.setNome(rst.getString(2));
+                        ultima = categoria;
+                        categorias.add(categoria);
+                    }
+                    Produto produto = new Produto();
+                    produto.setId(rst.getInt(3));
+                    produto.setNome(rst.getString(4));
+                    produto.setDescricao(rst.getString(5));
+                    ultima.adicionar(produto);
+                }
+            }
+
+        }
+        return categorias;
     }
 }
